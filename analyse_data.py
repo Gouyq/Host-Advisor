ab3deed69ed734988ecd3bd265b36416de8104ab
Corentinver <56277702+Corentinver@users.noreply.github.com>
2021-10-25 10:41:01 +0200

    Update analyse_data.py

--- analyse_data.py
+++ analyse_data.py
@@ -1,5 +1,15 @@
-from pyspark.sql.types import StructType
+  # Recuperation des meilleurs logements (note superieure ou egale a 4)
+  dataframeReferenceListing = dataframe.filter(dataframe.review_scores_rating >= 4)
 
+  # Recuperation des logements qui ont besoin d'aide (note strictement inferieure a 4)
+  dataframeListingToHelp = dataframe.filter(dataframe.review_scores_rating < 4)
+import sys
+
+from pyspark import SparkContext, SparkConf
+from pyspark.sql.session import SparkSession
+from pyspark.sql.types import StructType
+from pyspark.sql.functions import col, avg
+from pyspark.sql.types import IntegerType,BooleanType,DateType
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -10,40 +20,43 @@ if __name__ == "__main__":
   sc = SparkContext(conf=conf)
   spark = SparkSession(sc)
 
-  dataframe = spark.read.csv("/user/formation29/airbnb_clean.csv", header=True)
+  dataframe = spark.read.csv("/user/formation29/airbnb_clean.csv", header=True,inferSchema =True)
 
+  dataframe.withColumn("price",dataframe.price.cast(BooleanType()))
   # Recuperation des meilleurs logements (note superieure ou egale a 4)
   dataframeReferenceListing = dataframe.filter(dataframe.review_scores_rating >= 4)
 
   # Recuperation des logements qui ont besoin d'aide (note strictement inferieure a 4)
   dataframeListingToHelp = dataframe.filter(dataframe.review_scores_rating < 4)
 
+  
   # Initialisation du tableau qu'on enregistrera en CSV a la fin du programme
   listingsToHelp = []
 
   listingsImprovements = []
   # On boucle sur la liste des annonces a aider
-  for listing in dataframeListingToHelp:
-
+  for listing in dataframeListingToHelp.collect():
+    print(dict(dataframeListingToHelp.dtypes))
+    print(dict(dataframeReferenceListing.dtypes))
     # Initialisation et construction de l'objet qu'on inserera dans listingsToHelp
     listingToHelp = {}
     listingToHelp['listing_id'] = listing.id
     listingToHelp['listing_name'] = listing.name
     listingToHelp['listing_url'] = listing.listing_url
-    listingToHelp['host_id'] = listing.host_id
     listingToHelp['host_name'] = listing.host_name
     listingToHelp['reference_listing_url'] = []
 
-
+    print(dataframeReferenceListing.select(avg("price")).collect()[0][0])
     # On recupere les logements similaires dans la liste des logements de reference
     dataframeSimilarListing = dataframeReferenceListing.filter(
-        (0.9*dataframeReferenceListing.price < listing.price < 1.1*dataframeReferenceListing.price) &
+        (dataframeReferenceListing.price < 1.1*listing.price) &
+        (dataframeReferenceListing.price > 0.9*listing.price) &
         (dataframeReferenceListing.neighbourhood_cleansed == listing.neighbourhood_cleansed) &
-        (dataframeReferenceListing.neighbourhood == listing.neighbourhood) &
-        (dataframeBestHost.accommodates == listing.accommodates) &
-        (dataframeBestHost.room_type == listing.room_type) &
-        (dataframeBestHost.bedrooms == listing.bedrooms)
+        (dataframeReferenceListing.accommodates == listing.accommodates) &
+        (dataframeReferenceListing.room_type == listing.room_type) &
+        (dataframeReferenceListing.bedrooms == listing.bedrooms)
     )
+
 
     if listing.host_has_profile_pic is not True:
       listingsImprovements.append({"listing_id": listing.id, "improvement_id": 5})
